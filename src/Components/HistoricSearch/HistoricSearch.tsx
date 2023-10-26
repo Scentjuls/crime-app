5fbf2d485aab2e610e70923a8932e6cb561f217f
Scentjuls <chidynwanya11@gmail.com>
2023-10-26 13:03:42 +0100

    created base components/pages

---
+++ src/Components/HistoricSearch/HistoricSearch.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HistoricSearch = () => {
+  return <div>HistoricSearch</div>;
+};
---
+++ src/Pages/DataView/DataView.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const DataView = () => {
+  return <div>DataView</div>;
+};
